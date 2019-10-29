e022ce6725baef8e6ed17db9b4a0de1c98d21c70
rbernatovic <57090199+rbernatovic@users.noreply.github.com>
2019-10-29 12:49:14 +0200

    penkta

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <html>
     <head>
-        <title>PHP lydes ir <?php print date('Y:m:d', strtotime ("+1 day"))?>;</title>
+        <title>Komentarai</title>
     </head>
     <body>
-        <h1><b>Rafal</b> - PHP su manim buvo ir <?php print date ('Y:m:d h:i:s', strtotime ("-1 hour"))?></h1>
-        <p><?php print date('Y', strtotime ("+1year"))?> ne uz kalnu!</p>
+        <h1> Dainu Tekstai</h1>
+        <h2> Paragrafo Daina </h2>
+        <?php //<p> As toks vienisas jaunas paragrafas... </p>
+        <!--
+        <p> Mindau svetimo kodo gatves...</p>
+        -->
     </body>
 </html>
