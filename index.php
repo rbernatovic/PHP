9a2bda07d306273f4a407c2d724258b9e7852a7d
rbernatovic <57090199+rbernatovic@users.noreply.github.com>
2019-10-29 10:20:40 +0200

    pirmauzduotis

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
-<?php
-print 'veikia!';
+<html>
+    <head>
+        <title>Aš valdau HTML!</title>
+    </head>
+    <body>
+        <h1>Rafal - HTML asas</h1>
+        <p>Moku kurti statinį turinį</p>
+    </body>
+</html>
