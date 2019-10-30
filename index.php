1d65e94f27951686bed1ad598ef6c2964134e43e
rbernatovic <57090199+rbernatovic@users.noreply.github.com>
2019-10-30 10:14:51 +0200

    uzd8

--- index.php
+++ index.php
@@ -1,13 +1,26 @@
 <html>
     <head>
-        <title>Komentarai</title>
+        <title></title>
     </head>
     <body>
-        <h1> Dainu Tekstai</h1>
-        <h2> Paragrafo Daina </h2>
-        <?php //<p> As toks vienisas jaunas paragrafas... </p>?>
-        <!--
-        <p> Mindau svetimo kodo gatves...</p>
-        -->
+        <style>
+            body {
+                background-color: rgb(<?php print rand(0,255).','.rand(0,255).','.rand(0,255)?>);
+                width: 100vw;
+                height: 100vh;  
+            }
+            h1 {
+                font-size: <?php print rand(10,60)?>px;
+            }
+            p {
+                color: rgb(<?php print rand(0,255).','.rand(0,255).','.rand(0,255)?>);
+            }
+        </style>
+        <h1>
+            As keiciu savo dydi!
+        </h1>
+        <p>
+            As keiciu savo spalva!
+        </p>
     </body>
 </html>
