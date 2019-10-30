f3f6b917542ec66ba4d9fc1634f898b2df225a36
rbernatovic <57090199+rbernatovic@users.noreply.github.com>
2019-10-30 11:26:54 +0200

    bomba

--- index.php
+++ index.php
@@ -1,26 +1,19 @@
 <html>
     <head>
-        <title></title>
+        <title>bomba</title>
+        <style>
+            img {
+              
+               width: 1<?php print date('s')?>;
+               height: 1<?php print date('s')?>;
+            }
+            
+        </style>
     </head>
     <body>
-        <style>
-            body {
-                background-color: rgb(<?php print rand(0,255).','.rand(0,255).','.rand(0,255)?>);
-                width: 100vw;
-                height: 100vh;  
-            }
-            h1 {
-                font-size: <?php print rand(10,60)?>px;
-            }
-            p {
-                color: rgb(<?php print rand(0,255).','.rand(0,255).','.rand(0,255)?>);
-            }
-        </style>
-        <h1>
-            As keiciu savo dydi!
-        </h1>
-        <p>
-            As keiciu savo spalva!
-        </p>
+        <div class="flex">
+        <img src="assets\images\bomb.png">
+        <p><?php print date('s')?></p>
+        </div>
     </body>
 </html>
