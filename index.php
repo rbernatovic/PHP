acb61c3598bdcb1a0c98c6d6efd96a747d11c8ff
rbernatovic <57090199+rbernatovic@users.noreply.github.com>
2019-10-31 12:12:38 +0200

    siuksles2

--- index.php
+++ index.php
@@ -3,18 +3,21 @@
      $bin_heap_vol = rand(1,100);
      $trash_per_day = 15;
      $visas = $bin_vol + $bin_heap_vol;
-     $days = floor($visas / $trash_per_day);
+     $days = floor($visas / $trash_per_day); 
      
-             
+     $h1 = "Siuksliu prognoze";
+     $p_1 = "Turima siuksline $bin_vol litru";
+     $p_2 = "Zmona nieko nesako kol kaupas nevirsija $bin_heap_vol litru";
+     $h3 = "Isvada: Nieko nedarysiu $days dienas";
 ?>
 <html>
     <head>
         <title>siuksles</title>
     </head>
     <body>
-        <h1>Siuksliu prognoze</h1>
-        <p>Turima siuksline <?php print $bin_vol?> litru</p>
-        <p>Zmona nieko nesako kol kaupas nevirsija <?php print $bin_heap_vol?></p>
-        <h3>Isvada: Nieko nedarysiu <?php print $days?> dienu</h3>
+        <h1><?php print $h1?></h1>
+        <p><?php print $p_1?></p>
+        <p><?php print $p_2?></p>
+        <h3><?php print $h3?></h3>
     </body>
 </html>
