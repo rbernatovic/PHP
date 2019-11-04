13cfa63e6be1991dd243f512ff69f4426ae931d3
rbernatovic <57090199+rbernatovic@users.noreply.github.com>
2019-11-04 10:45:28 +0200

    uzduotis1

--- index.php
+++ index.php
@@ -1,25 +1,28 @@
 <html>
     <head>
         <title>uzduotis</title>
-        <?php 
-$grizai_velai = rand(0,1);
-$grizai_isgeres = rand(0,1);
-if ($grizai_velai && $grizai_isgeres) {
-    print 'Grizai velai ir isgeres';
-}
-if ($grizai_isgeres && !$grizai_velai) {
-    print 'grizai isgeres';
-}
-if ($grizai_velai && !$grizai_isgeres) {
-    print 'grizai velai';
-}
-if (!$grizai_velai && !$grizai_isgeres) {
-    print 'nieko nepadarei';
-}
-?>
+        <?php
+        $grizai_velai = rand(0, 1);
+        $grizai_isgeres = rand(0, 1);
+        if ($grizai_velai && $grizai_isgeres) {
+            $situacija = 'Grizai velai ir isgeres';
+        }
+        if ($grizai_isgeres && !$grizai_velai) {
+            $situacija = 'Grizai isgeres';
+        }
+        if ($grizai_velai && !$grizai_isgeres) {
+            $situacija = 'Grizai velai';
+        }
+        if (!$grizai_velai && !$grizai_isgeres) {
+            $situacija = 'Nieko nepadarei';
+        }
+        $Buitine_sk = 'Buitine skaiciuokle:';
+        ?>
+
     </head>
     <body>
-        <h1>Buitine skaiciuokle</h1>
+        <h1><?php print $Buitine_sk ?></h1>
+        <p><?php print $situacija ?></p>
     </body>
 </html>
 
