1e3534b1421a4f815679fb96fae47de75b4cd013
rbernatovic <57090199+rbernatovic@users.noreply.github.com>
2019-11-05 11:01:40 +0200

    ciklas1

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <?php
-$x = rand(1, 100);
+$kates = rand(1, 3);
+$sunys = rand(1, 3);
+$katasuniai = 0;
 
-for ($y = 1; $y < 100; $y = $x) {
-    break;
+for ($x = 1; $x <= $kates; $x++) {
+    for ($y = 1; $y <= $sunys; $y++) {
+        $pavyko = rand(0, 1);
+        if ($pavyko) {
+            $katasuniai++;
+            break;
+        }
+    }
 }
+$h1 = 'Katasuniu iseiga';
+$h2 = "Dalyvavo $kates kates ir $sunys sunys";
+$h3 = "Katasuniu iseiga: $katasuniai katasuniai";
 ?>
 <html>
     <head>
         <title>ciklas</title>
     </head>
     <body>
-        <p><?php print "Tai yra $x - tasis ciklas"; ?></p>
+        <h1><?php print $h1 ?></h1>
+        <h2><?php print $h2 ?></h2>
+        <h3><?php print $h3 ?></h3>
     </body>
 </html>
