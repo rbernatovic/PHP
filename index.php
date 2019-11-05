ca2271eecd4c3df96026ba36dc297bdf5cfe7dfb
rbernatovic <57090199+rbernatovic@users.noreply.github.com>
2019-11-05 12:03:29 +0200

    ciklas1

--- index.php
+++ index.php
@@ -1,28 +1,26 @@
 <?php
-$kates = rand(1, 3);
-$sunys = rand(1, 3);
-$katasuniai = 0;
+$months = 12;
+$wallet = 1000;
+$month_income = 700;
 
-for ($x = 1; $x <= $kates; $x++) {
-    for ($y = 1; $y <= $sunys; $y++) {
-        $pavyko = rand(0, 1);
-        if ($pavyko) {
-            $katasuniai++;
-            break;
-        }
+
+for ($x = 1; $x <= $months; $x++) {
+    $month_expenses = rand(600, 3000);
+    if ($wallet > 0) {
+        $wallet = $wallet + $month_income - $month_expenses;
+    } else {
+        break;
     }
 }
-$h1 = 'Katasuniu iseiga';
-$h2 = "Dalyvavo $kates kates ir $sunys sunys";
-$h3 = "Katasuniu iseiga: $katasuniai katasuniai";
+$h1 = 'Wallet Forecast:';
+$h2 = "Atsargiai, $x menesi gali baigtis pinigai!";
 ?>
 <html>
     <head>
-        <title>ciklas</title>
+        <title>Uzd3</title>
     </head>
     <body>
         <h1><?php print $h1 ?></h1>
         <h2><?php print $h2 ?></h2>
-        <h3><?php print $h3 ?></h3>
     </body>
 </html>
