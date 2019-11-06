b3740f2762fc97b876a728f30b333a0353a6f1b8
rbernatovic <57090199+rbernatovic@users.noreply.github.com>
2019-11-06 12:17:45 +0200

    depr

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <?php
-$petrol_in_tank = rand(1, 50);
+$months = 24;
+$car_price_new = 30000;
+$car_price_used = $car_price_new;
+$depreciation = 0.02;
+$santaupos = 15000;
 
 
-$diesel_limit = ($petrol_in_tank + ($petrol_in_tank * 0.1)) * 0.1;
-for ($i = 0; $i < 100; $i++) {
-    if ($i > $diesel_limit) {
+for ($i = 1; $i <= 100; $i++) {
+    $car_price_used -= ($car_price_used * $depreciation);
+    if ($car_price_used < 15000) {
         break;
     }
 }
-$h1 = 'Pripylei dyzelio i benz. masina?';
-$h2 = "Benzino buvo: $petrol_in_tank l";
-$h3 = "Max dyzelio riba: $i l";
+$car_price_used = round($car_price_used);
+$depr_eur = $car_price_new - $car_price_used;
+$depr_perc = round($depr_eur * 100 / $car_price_new, 1);
+
+
+$h1 = 'Kiek nuvertes masina?';
+$h2 = "Naujos masinos kaina: $car_price_new";
+$h3 = "Masina galesi nusipirkti po $i men, kai jos verte bus $car_price_used eur";
+$h4 = "Masina nuvertes $depr_perc proc.";
 ?>
 <html>
     <head>
@@ -20,5 +30,6 @@ $h3 = "Max dyzelio riba: $i l";
         <h1><?php print $h1 ?></h1>
         <h2><?php print $h2 ?></h2>
         <h3><?php print $h3 ?></h3>
+        <h4><?php print $h4 ?></h4>
     </body>
 </html>
