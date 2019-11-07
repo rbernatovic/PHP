e664d95d916b45e2dc89c9ce8af7e722c4feed47
rbernatovic <57090199+rbernatovic@users.noreply.github.com>
2019-11-07 11:44:17 +0200

    masyvas

--- index.php
+++ index.php
@@ -1,8 +1,45 @@
-<?php 
-
-$i = ['Kiausinis', 'Baklazanas', 'Obuolys'];
-var_dump($i);
-
+<?php
+$maisto_prekes = [
+    [
+        'Lentyna1' => [
+            'Pozicija1' => [
+                'produktas' => 'kiausiniai',
+                'kiekis' => 2
+            ],
+            'Pozicija2' => [
+                'produktas' => 'grietine',
+                'kiekis' => 1
+            ],
+            'Pozicija3' => [
+                'produktas' => 'jogurtas',
+                'kiekis' => 3
+            ],
+            'Pozicija4' => [
+                'produktas' => 'zuvis',
+            ],
+        ],
+    ],
+    [
+        'Lentyna2' => [
+            'Pozicija1' => [
+                'produktas' => 'pienas',
+                'kiekis' => 1
+            ],
+            'Pozicija2' => [
+                'produktas' => 'kefyras',
+            ],
+            'Pozicija3' => [
+                'produktas' => 'degtine',
+                'kiekis' => 2
+            ],
+            'Pozicija4' => [
+                'produktas' => 'kecupas',
+                'kiekis' => 2
+            ],
+        ],
+    ],
+];
+var_dump($maisto_prekes)
 ?>
 <html>
     <head>
