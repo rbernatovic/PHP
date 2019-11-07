ba07c7a1bda3a7b38099515ff09076c0c5721ffe
rbernatovic <57090199+rbernatovic@users.noreply.github.com>
2019-11-07 11:51:56 +0200

    masyvas

--- index.php
+++ index.php
@@ -1,45 +1,41 @@
 <?php
 $maisto_prekes = [
     [
-        'Lentyna1' => [
-            'Pozicija1' => [
-                'produktas' => 'kiausiniai',
-                'kiekis' => 2
-            ],
-            'Pozicija2' => [
-                'produktas' => 'grietine',
-                'kiekis' => 1
-            ],
-            'Pozicija3' => [
-                'produktas' => 'jogurtas',
-                'kiekis' => 3
-            ],
-            'Pozicija4' => [
-                'produktas' => 'zuvis',
-            ],
+        [
+            'produktas' => 'kiausiniai',
+            'kiekis' => 2
+        ],
+        [
+            'produktas' => 'grietine',
+            'kiekis' => 1
+        ],
+        [
+            'produktas' => 'jogurtas',
+            'kiekis' => 3
+        ],
+        [
+            'produktas' => 'zuvis',
         ],
     ],
     [
-        'Lentyna2' => [
-            'Pozicija1' => [
-                'produktas' => 'pienas',
-                'kiekis' => 1
-            ],
-            'Pozicija2' => [
-                'produktas' => 'kefyras',
-            ],
-            'Pozicija3' => [
-                'produktas' => 'degtine',
-                'kiekis' => 2
-            ],
-            'Pozicija4' => [
-                'produktas' => 'kecupas',
-                'kiekis' => 2
-            ],
+        [
+            'produktas' => 'pienas',
+            'kiekis' => 1
+        ],
+        [
+            'produktas' => 'kefyras',
+        ],
+        [
+            'produktas' => 'degtine',
+            'kiekis' => 2
+        ],
+        [
+            'produktas' => 'kecupas',
+            'kiekis' => 2
         ],
     ],
 ];
-var_dump($maisto_prekes)
+var_dump($maisto_prekes);
 ?>
 <html>
     <head>
