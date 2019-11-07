aa0e73e90825815f20259fa502af8d795c4d80e6
rbernatovic <57090199+rbernatovic@users.noreply.github.com>
2019-11-07 13:01:37 +0200

    masyvas

--- index.php
+++ index.php
@@ -2,27 +2,27 @@
 $trasa = [
     'posukis1' => [
         'puse' => 'desine',
-        'kampas' => '70°'
+        'kampas' => 70
     ],
     'posukis2' => [
         'puse' => 'kaire',
-        'kampas' => '15°'
+        'kampas' => 15
     ],
     'posukis3' => [
         'puse' => 'desine',
-        'kampas' => '100°'
+        'kampas' => 100
     ],
     'posukis4' => [
         'puse' => 'kaire',
-        'kampas' => '130°'
+        'kampas' => 130
     ],
     'posukis5' => [
         'puse' => 'desine',
-        'kampas' => '180°'
+        'kampas' => 180
     ],
     'posukis' => [
         'puse6' => 'desine',
-        'kampas' => '30°'
+        'kampas' => 30
     ],
 ];
 var_dump($trasa)
